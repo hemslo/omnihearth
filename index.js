910482939a99866bf182ab746d1fd6d68a56f847
Hemslo Wang <hemslo.wang@gmail.com>
2015-05-27 23:18:00 +0800

    Fix sytle

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ require('crash-reporter').start();
 let mainWindow = null;
 
 app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function () {
-	mainWindow = new BrowserWindow({
-		width: 600,
-		height: 400,
-		resizable: false
-	});
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    resizable: false
+  });
 
-	mainWindow.loadUrl(`file://${__dirname}/index.html`);
+  mainWindow.loadUrl(`file://${__dirname}/index.html`);
 
-	mainWindow.on('closed', function () {
-		// deref the window
-		// for multiple windows store them in an array
-		mainWindow = null;
-	});
+  mainWindow.on('closed', function () {
+    // deref the window
+    // for multiple windows store them in an array
+    mainWindow = null;
+  });
 });
