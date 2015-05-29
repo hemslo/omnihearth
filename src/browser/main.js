d12f961ca9963d2e381c255e16baffd8262db692
Qi Chen <aseaday@gmail.com>
2015-05-29 22:19:10 +0800

    Mofify main.js

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,31 +1,31 @@
 'use strict';
 const app = require('app');
-const path = require('path');
 const BrowserWindow = require('browser-window');
+const path = require('path');
 
 // report crashes to the Electron project
 require('crash-reporter').start();
 
 // prevent window being GC'd
 let mainWindow = null;
-let resource_path = path.resolve(__dirname, '..', '..', 'resource');
+let static_path = path.resolve(__dirname, '..', '..', 'static');
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('ready', function () {
+app.on('ready', function() {
   mainWindow = new BrowserWindow({
     width: 600,
     height: 400,
     resizable: false
   });
 
-  mainWindow.loadUrl(`file://${resource_path}/index.html`);
+  mainWindow.loadUrl(`file://${static_path}/index.html`);
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // deref the window
     // for multiple windows store them in an array
     mainWindow = null;
