796a9057f553d2806949a2f6f5b8f564e6854142
Hemslo Wang <hemslo.wang@gmail.com>
2015-06-02 01:59:22 +0800

    Better logs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 'use strict';
 var path = require('path');
 var LogStream = require('./log-stream');
-var LogParser = require('./log-parser.js');
+var LogParser = require('./log-parser');
 
 var logpath = path.join(process.env.HOME, 'Library', 'Logs', 'Unity', 'Player.log');
 var log_stream = new LogStream(logpath);
 var log_parser = new LogParser();
 log_stream.watch().on('new', function (data) {
-  console.log(data);
-  console.log(log_parser.parse(data));
+  let result = log_parser.parse(data);
+  if (result !== null) {
+      console.log(result);
+  }
 });
